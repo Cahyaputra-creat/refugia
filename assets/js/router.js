b04204fb11bad433d3d7f4c05dbb256c46e284ad
unknown <ondaon47@gmail.com>
2026-08-05 18:09:02 +0700

    QA Final Fix: fix CSP for weather API, add SPA routes, fix reinitializePageScripts method names

--- assets/js/router.js
+++ assets/js/router.js
@@ -215,14 +215,8 @@ const RefugiaRouter = (() => {
         if (typeof window.initHamburger === 'function') {
             window.initHamburger();
         }
-        if (typeof RefugiaFasilitas !== 'undefined' && typeof RefugiaFasilitas.init === 'function') {
-            RefugiaFasilitas.init();
-        }
-        if (typeof RefugiaFAQ !== 'undefined' && typeof RefugiaFAQ.init === 'function') {
-            RefugiaFAQ.init();
-        }
-        if (typeof RefugiaKontak !== 'undefined' && typeof RefugiaKontak.init === 'function') {
-            RefugiaKontak.init();
+        if (typeof RefugiaFasilitas !== 'undefined' && typeof RefugiaFasilitas.initSlider === 'function') {
+            RefugiaFasilitas.initSlider();
         }
     }
 
